353a740b09595130e0137daac05a55f74c76438c
mslowikowski <you@example.com>
2020-04-09 11:21:29 -0400

    feat: add default maximum enty length commandline argument; fix: environment console width

--- lastpass-csv/lastpass-csv.py
+++ lastpass-csv/lastpass-csv.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import argparse
+import os
+
+os.environ['COLUMNS'] = "80"
+defaultMaximum = 1000
 
 def main():
-    parser = argparse.ArgumentParser(description='parse a lastpass csv export and transform it as desired')
-    parser.add_argument('--input', help="lastpass csv file to be parsed", required=True)
-    parser.add_argument('--output', help="file to save the transformed data", required=True)
-
+    parser = argparse.ArgumentParser(description='Parse a lastpass csv export and transform it as desired.', )
+    parser.add_argument('-m', '--maximum', help=f"The maximum length of an entry (default = {defaultMaximum})", required=False, default=defaultMaximum)
+    parser.add_argument('-i', '--input', help="The LastPass csv file to be parsed", required=True)
+    parser.add_argument('-o', '--output', help="The output file to save the transformed data", required=True)
     # get the args
     args = parser.parse_args()
 
